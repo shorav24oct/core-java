b5497211a83b556fcbca4939baf1cf117f48d9e4
shoravs <sharmasaurav142@gmail.com>
2022-07-31 16:31:43 +0530

    try catch finally block execution

--- ExceptionHandling/src/com/eh/Test.java
+++ ExceptionHandling/src/com/eh/Test.java
@@ -3,7 +3,16 @@ package com.eh;
 public class Test {
 
 	public static void main(String[] args) {
-		
+		try {
+			System.out.println("Inside try block");
+			System.out.println();
+			System.out.println(10/0);
+		} catch (NullPointerException nullPointerException) {
+			System.out.println("Inside catch block");
+		} finally {
+			System.out.println("Inside final block");
+			System.out.println("------------------");
+		}
 	}
 
 }
